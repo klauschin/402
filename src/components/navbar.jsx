c578d1b02e5a9e9757f9266ffa83e5f1140b0f75
chinklaus <chinklaus@gmail.com>
2018-08-18 18:38:52 +0800

    add user section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,17 @@ class NavBar extends Component {
     console.log("NavBar - Rendered");
     return (
       <nav className="navbar">
-        <a className="navbar-content" href="#">
-          <h1>402</h1>
-          <span className="navbar-span" />
-        </a>
+        <div className="navbar__content">
+          <a className="navbar__content-a" href="#">
+            <h1>402</h1>
+          </a>
+          <div className="user__section">
+            <figure className="user__section-img">
+              <img src="./img/IMG_5544.JPG" alt="user-img" />
+            </figure>
+            <span>Brian Chen</span>
+          </div>
+        </div>
       </nav>
     );
   }
